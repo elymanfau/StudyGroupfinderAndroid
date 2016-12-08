4dd87014dffc92df5d5a6cd2dd997109791f3a64
Dgabay2015 <dgabay2015@fau.edu>
2016-12-08 17:34:47 -0500

    tested comment

--- app/src/main/java/com/example/dalexi1312/studygroupfinder/MainActivity.java
+++ app/src/main/java/com/example/dalexi1312/studygroupfinder/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private Button mChooseSignInButton;
 
 //this is a comment for testing
+    //now another comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

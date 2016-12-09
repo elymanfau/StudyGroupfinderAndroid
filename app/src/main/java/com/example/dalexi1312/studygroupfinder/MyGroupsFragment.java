dc284e52060f6ef541e9a8a6721a310c34f36c5c
elymanfau <elyman@fau.edu>
2016-12-08 20:40:40 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/dalexi1312/studygroupfinder/MyGroupsFragment.java
@@ -0,0 +1,24 @@
+package com.example.dalexi1312.studygroupfinder;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MyGroupsFragment extends Fragment {
+    public MyGroupsFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_groups, container, false);
+    }
+}
